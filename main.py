87dbeb93f8d258be07c51c85c6b2fe96bb1ddc80
braden <leachbr787@northwested.org>
2024-09-09 14:16:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print('Hello world!')
-print('My Name is Braden!')
-print('Good afternoon!')
+
+#Braden Leach
+#06 SEP 2024
+# Printing in Python
+
+#p1
+person_name = "david"
+print(f'hello, {person_name} would you like to learn python?')
+#p2
+
+print("Rosa Parks once said, each person must live their lives as a model for themselves")
+#p3
+quote = 'each person must live their lives as a model for themselves'
+famous_person = 'Rosa Parks'
+full_text = f"{famous_person}{quote}"
+message =f"{famous_person} once said, {quote}"
+print(message)
+
+
+
+
